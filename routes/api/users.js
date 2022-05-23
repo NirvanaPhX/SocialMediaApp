5cf5fc81d33cf28d517551829b4809908e706b60
Li <xiaomeng.li@edu.sait.ca>
2022-05-22 22:17:01 -0600

    Created validation for user registeration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Users API");
-});
+// @route POST api/users
+// @desc Register user
+// @access Public
+router.post(
+  "/",
+  body("name").not().isEmpty(),
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+  }
+);
 
 module.exports = router;
